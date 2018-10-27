441690bc4d3508dc2da4b9a0b6acc37b621ed958
BAMHiggin <bamhiggin@gmail.com>
2018-10-27 00:34:03 -0500

    added win/loss game logic and buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,5 @@
 
-var randomNumber
-$(document).ready(function() { // event signals that DOM is ready
+$(document).ready(function () { // event signals that DOM is ready
 
     //global variables
     var randomNumber = ""; //anything random needs string
@@ -12,13 +11,66 @@ $(document).ready(function() { // event signals that DOM is ready
     var redGem = "";
     var totalPoints = 0;
 
-    gameStart() {
-        var randomNumber()
+    // formulas for random numbers
+    function gameStart() {
+        randomNumber = Math.floor(Math.random() * 101) + 19;
+        blueGem = Math.floor(Math.random() * 11) + 1;
+        redGem = Math.floor(Math.random() * 11) + 1;
+        yellowGem = Math.floor(Math.random() * 11) + 1;
+        greenGem = Math.floor(Math.random() * 11) + 1;
+
+    };
+    gameStart();
+
+
+    //win or loss logic
+    if (totalPoints === randomNumber) {
+        alert("WIN");
+        wins++;
+        reset();
+
+    } else if (totalPoints > randomNumber) {
+        alert("You Lose!");
+        losses++;
+        reset();
+    }
+
+
+
+    // gem button click events
+    $("#blueGem").click(function() {
+        totalPoints += blueGem; //adding mystery gem value to total points score
+        console.log(blueGem);
+    });
+    $("#redGem").click(function() {
+        totalPoints += redGem;
+        console.log(redGem);
+    });
+    $("#yellowGem").click(function() {
+        totalPoints += yellowGem; 
+        console.log(yellowGem);
+    });
+    $("#greenGem").click(function() {
+        totalPoints += greenGem; 
+        console.log(greenGem);
     });
 
 
 
 
+
+
+    //for reset after win/loss is triggered
+    function reset() {
+        totalPoints = 0;
+        gameStart();
+    }
+
+//testing space
+    console.log(blueGem);
+    console.log(redGem);
+    console.log(greenGem);
+    console.log(yellowGem);
 
 
 
